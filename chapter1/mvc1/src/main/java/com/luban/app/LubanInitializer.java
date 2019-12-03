650705f454332cc6c567fe0273b1d0518d04649f
wangjm <wangjm@zhiyuaniot.com>
2019-12-03 15:08:33 +0800

    spring servlet init rename

--- chapter1/mvc1/src/main/java/com/luban/app/LubanInitializer.java
+++ chapter1/mvc1/src/main/java/com/luban/app/LubanInitializer.java
@@ -1,17 +1,14 @@
 package com.luban.app;
 
 import com.luban.app.web.SpringServlet;
-import org.springframework.web.WebApplicationInitializer;
 
 import javax.servlet.ServletContainerInitializer;
 import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
-import javax.servlet.annotation.HandlesTypes;
 import java.util.Set;
-public class Lubannitializer implements ServletContainerInitializer {
+public class LubanInitializer implements ServletContainerInitializer {
     @Override
-    public void onStartup(Set<Class<?>> c, ServletContext ctx) throws ServletException {
+    public void onStartup(Set<Class<?>> c, ServletContext ctx) {
         ServletRegistration.Dynamic registration=ctx.addServlet("xx",new SpringServlet());
         registration.addMapping("/");
     }
